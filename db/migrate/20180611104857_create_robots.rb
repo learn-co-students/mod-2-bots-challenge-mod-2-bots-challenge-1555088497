8df7454a20b37a088732694e247dfcdd45416ffb
rishter <rishikesh@flatironschool.com>
2018-06-11 13:02:07 +0100

    added initial db structure

---
+++ db/migrate/20180611104857_create_robots.rb
@@ -0,0 +1,10 @@
+class CreateRobots < ActiveRecord::Migration[5.1]
+  def change
+    create_table :robots do |t|
+      t.string :name
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
